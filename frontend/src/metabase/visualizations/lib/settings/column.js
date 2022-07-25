3a0346b6133c8cc6dba7cd0e3e0ee5111fbed620
gitstart <gitstart@users.noreply.github.com>
2022-07-25 07:46:31 +0000

    Offer symbol_native currency format as an option
    Co-authored-by: gitstart <gitstart@gitstart.com>
    Co-authored-by: gitstart <gitstart@users.noreply.github.com>
    Co-authored-by: Rubens Rafael <70234898+RubensRafael@users.noreply.github.com>
    Co-authored-by: Nitesh Singh <nitesh.singh@gitstart.dev>
    Co-authored-by: Toledodev <rafael.toledo@engenharia.ufjf.br>
    Co-authored-by: Gh0sT <coolmagnas@gmail.com>
    Co-authored-by: GitStart <1501599+gitstart@users.noreply.github.com>
    Co-authored-by: Benjamin Mayanja <vibenjamin6@gmail.com>

--- frontend/src/metabase/visualizations/lib/settings/column.js
+++ frontend/src/metabase/visualizations/lib/settings/column.js
@@ -320,6 +320,7 @@ export const NUMBER_COLUMN_SETTINGS = {
       const symbol = getCurrencySymbol(c);
       const code = getCurrency(c, "code");
       const name = getCurrency(c, "name");
+      const symbol_native = getCurrencySymbol(c, true);
       return {
         options: [
           ...(symbol !== code
@@ -337,6 +338,10 @@ export const NUMBER_COLUMN_SETTINGS = {
           {
             name: t`Name` + ` ` + `(${name})`,
             value: "name",
+          },
+          {
+            name: t`Symbol Native` + ` ` + `(${symbol_native})`,
+            value: "symbol_native",
           },
         ],
       };
@@ -427,6 +432,9 @@ export const NUMBER_COLUMN_SETTINGS = {
       ) {
         if (settings["currency_style"] === "symbol") {
           return getCurrencySymbol(settings["currency"]);
+        }
+        if (settings["currency_style"] === "symbol_native") {
+          return getCurrencySymbol(settings["currency"], true);
         }
         return getCurrency(settings["currency"], settings["currency_style"]);
       }
